26a03206c029ceeb37f216c52aaedbe1fccc3107
MikeLidner <maycon.stos.lima@gmail.com>
2025-12-10 19:39:20 -0300

    final

---
+++ back/exemp.js
@@ -0,0 +1,12 @@
+//app.post("/upload", upload.single("foto"), (req, res) => {
+    //const fs = require("fs");
+
+    //const { nome, localizacao, informacoes } = req.body;
+    //const imageBuffer = fs.readFileSync(req.file.path);
+
+    //const sql = "INSERT INTO lugares (nome, localizacao, informacoes, foto) VALUES (?, ?, ?, ?)";
+    //db.query(sql, [nome, localizacao, informacoes, imageBuffer], (err, result) => {
+        //if (err) throw err;
+        //res.send("Imagem salva no banco!");
+    //});
+//});
